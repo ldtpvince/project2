8f528c4e4f3e7f93bbe28770bb7b63fd94a9b1a9
naeko-eulb <62087687+naeko-eulb@users.noreply.github.com>
2020-06-19 14:53:34 +0700

    Chỉnh lại 1 dòng comment tại Convert

--- project2.asm
+++ project2.asm
@@ -859,7 +859,7 @@ Convert:
 	sw	$ra, 0($sp)
 	sw	$a0, 16($sp)
 	sw	$a1, 20($sp)
-	#Lay ngay, thang, nam vao $t0, $t1, $t2
+	#Lay ngay, thang, nam lưu vào stack
 	jal	Day
 	sw	$v0,4($sp)
 	jal	Month
